5fa191d9f3511224d8b700dd1b7940495f1cfce3
Seynath Thenura <117857144+seynath@users.noreply.github.com>
2025-01-24 12:20:28 +0530

    Delete server directory

--- server/utils/validateMongodbid.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require('mongoose');
-// const validateMongoDbId = (id) => {
-//   if (!mongoose.Types.ObjectId.isValid(id)) {
-//     throw new Error('Invalid ID');
-//   }
-// };
-// module.exports = validateMongoDbId;
