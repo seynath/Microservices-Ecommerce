f8f9286eac93cd2cbee677b34a6e4497b2cb7ef8
Seynath Thenura <117857144+seynath@users.noreply.github.com>
2025-01-24 12:20:13 +0530

    Delete client directory

--- client/src/routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoute = ({children}) =>{
-  const getItemFromLocalStorage = JSON.parse(localStorage.getItem("user"))
-  console.log(getItemFromLocalStorage?.token);
-  return getItemFromLocalStorage?.token !== undefined ? children : (<Navigate to="/login" replace={true}/> )
-}
